f8fe4e5b6a460e4c8bd233a2c3d22241588c00cb
Ar998-max <78679302+Ar998-max@users.noreply.github.com>
2023-04-02 21:39:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,8 +37,9 @@ def amazon_product_ret():
 
     headers = ({'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5)AppleWebKit/605.1.15 (KHTML, like Gecko)Version/12.1.1 Safari/605.1.15', 'Accept-Language': 'en-US, en;q=0.5'})
     proxies = {
-        "http": "64.225.4.12:9984",
-        "https": "64.225.4.12:9984"
+        "http": "2.56.119.93:5074",
+        "https": "2.56.119.93:5074",
+        
     }
     print(headers)
 
